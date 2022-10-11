65cc0fbb6f01a8930182da3762228df0ad5aec7c
anaip30 <anai30.pucic@gmail.com>
2022-10-11 08:31:31 +0200

    rijesenje

---
+++ zadatk1.js
@@ -0,0 +1,12 @@
+let b = ["LAlala je lalall "]
+let r=[]
+let brojac=0;
+let a = (l) => { 
+    for(let i=0; i<l.length;i++){ brojac++}
+ const bb= l.split(brojac/2)
+ console.log(l)
+ 
+}
+
+a(b)
+
