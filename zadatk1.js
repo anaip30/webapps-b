441de4f0213244d8fe00036d9b3c4261ca3601bd
anaip30 <anai30.pucic@gmail.com>
2022-10-11 08:42:50 +0200

    ispravak 1

--- zadatk1.js
+++ zadatk1.js
@@ -1,11 +1,11 @@
-let b = ["LAlala je lalall "]
+let b = ["LAlala je lalall "];
 let r=[]
 let brojac=0;
 let a = (l) => { 
-    for(let i=0; i<l.length;i++){ brojac++}
- const bb= l.split(brojac/2)
- console.log(l)
- 
+    const pola= Math.ceil(l.length/2);
+    const prvapol=l.slice(0,pola.length)
+    const drugapol=l.slice(pola.length)
+ console.log("Ispis:", l + "->" + drugapol) 
 }
 
 a(b)
